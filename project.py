b56347db0602ab425247b7cacea86129730ce0c1
mayukhalingam <mayukha296@gmail.com>
2019-11-24 12:58:58 +0530

    adding recursive loop

--- project.py
+++ project.py
@@ -2,6 +2,11 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import PySimpleGUI as sg
+import argparse
+import os
+from skimage.feature import hog
+from scipy import ndimage
+from skimage.restoration import denoise_nl_means
 
 # reading the input image
 img_input = cv2.imread("test_image.jpg",0)
@@ -49,59 +54,10 @@ def shape_selection(event, x, y, flags, param):
         ref_point.append((x, y))
 
         # draw a rectangle around the region of interest 
-        cv2.rectangle(image, ref_point[0], ref_point[1], (0, 255, 0), 2)
+	#cv2.rectangle(image, ref_point[0], ref_point[1], (0, 255, 0), 2)
         cv2.imshow("image", image)
 
 
-
-
-# load the image, clone it, and setup the mouse callback function 
-image = img
-clone = image.copy()
-cv2.namedWindow("image")
-cv2.setMouseCallback("image", shape_selection)
-
-
-# keep looping until the 'q' key is pressed 
-while True:
-        # display the image and wait for a keypress 
-    cv2.imshow("image", image)
-    key = cv2.waitKey(1) & 0xFF
-
-    # press 'r' to reset the window 
-    if key == ord("r"):
-        image = clone.copy()
-
-        # if the 'c' key is pressed, break from the loop 
-    elif key == ord("c"):
-        break
-
-if len(ref_point) == 2:
-    crop_img = clone[ref_point[0][1]:ref_point[1][1], ref_point[0][0]:
-                                                        ref_point[1][0]]
-    a=ref_point[0]
-    b=ref_point[1]
-    xt=(a[0]+b[0])//2
-    yt=(a[1]+b[1])//2
-    cv2.imshow("crop_img", crop_img)
-    cv2.waitKey(0)
-
-
-cv2.destroyAllWindows()
-
-layout = [
-    [sg.Text('Please enter the character id')],
-    [sg.Text('id', size=(15, 1)), sg.InputText()],
-    [sg.Submit(), sg.Cancel()]
-]
-
-window = sg.Window('ID-Window', layout)
-event, values = window.Read()
-window.Close()
-
-#################### Encoding Function for unicode Mapping ##################################
-
-#Encode function to map the given user input with its correponding unicode 
 def encode(ip):
     if(ip<6):
         op=str(0)+str(91)+str(ip+4)
@@ -145,13 +101,6 @@ def encode(ip):
     
     return op
 
-
-ip=values[0]
-ip=int(ip)
-op_code=encode(ip)
-print(op_code)
-
-##########NCC computation #######################
 def splitting(img):
     l,w = img.shape[0:2]
     l1 = int(l/2)
@@ -179,11 +128,6 @@ def splitting(img):
     
     return s
 
-
-# s1 existing template of the original letter
-# s2 windowed template at that particular location
-
-#Normalized correlation utility function 
 def correlation(img,x,y,s1):
     ans=[]
     s2=extract_template(img,x,y)
@@ -206,93 +150,181 @@ def correlation(img,x,y,s1):
 
 
 
-#Thresholding with respect to 
-templates = splitting(crop_img)
-mat = []
-for template in templates:
-    t_w = int(template.shape[0]/2)
-    t_h = int(template.shape[1]/2)
-    im_pad = np.pad(img,((t_w,t_w),(t_h,t_h)),'constant')
-    ret=cv2.matchTemplate(im_pad,template,cv2.TM_CCORR_NORMED)
-    filtered = ndimage.maximum_filter(ret, size=4)
-    mat.append(filtered)
+#done = True
+done=False
+
+while(done==False):
     
-s = np.array(mat[0])
-for i in range(1,len(mat)):
-    s = s+np.array(mat[i])
+    ref_point = [] 
+    crop = False
+
+# load the image, clone it, and setup the mouse callback function 
+    image = img
+    clone = image.copy()
+    cv2.namedWindow("image")
+    cv2.setMouseCallback("image", shape_selection)
+
+
+    # keep looping until the 'q' key is pressed 
+    while True:
+            # display the image and wait for a keypress 
+        cv2.imshow("image", image)
+        key = cv2.waitKey(1) & 0xFF
+
+        # press 'r' to reset the window 
+        if key == ord("r"):
+            image = clone.copy()
+
+            # if the 'c' key is pressed, break from the loop 
+        elif key == ord("c"):
+            break
+
+    if len(ref_point) == 2:
+        crop_img = clone[ref_point[0][1]:ref_point[1][1], ref_point[0][0]:
+                                                            ref_point[1][0]]
+        a=ref_point[0]
+        b=ref_point[1]
+        xt=(a[0]+b[0])//2
+        yt=(a[1]+b[1])//2
+        cv2.imshow("crop_img", crop_img)
+        cv2.waitKey(0)
+
+
+    cv2.destroyAllWindows()
+
+    layout = [
+        [sg.Text('Please enter the character id')],
+        [sg.Text('id', size=(15, 1)), sg.InputText()],
+        [sg.Text('Type yes if all the characters are marked', size=(5, 1)), sg.InputText()],
+       
+        [sg.Submit(), sg.Cancel()]
+    ]
+
+    window = sg.Window('ID-Window', layout)
+    event, values = window.Read()
+    window.Close()
+
     
-op=np.zeros(ret.shape)
-for i in range(ret.shape[0]):
-    for j in range(ret.shape[1]):
-        if(ret[i][j]>0.75):
-            op[i][j]=255
-
-plt.imshow(op,'gray')
-plt.show()
-
-############Clustering for close locations############
-t=10
-a=[]
-i=0
-
-while(i<index.shape[1]):
-    print(i)
-    mx=index[0][i]
-    my=index[1][i]
-    count=1
-    for j in range(i+1,index.shape[1]):
-        marginx=np.abs(index[0][i]-index[0][j])
-        marginy=np.abs(index[1][i]-index[1][j])
-        if(marginx<t and marginy<t):
-            mx=mx+index[0][j]
-            my=my+index[1][j]
-            count=count+1
-            step=j
-    print(count,step)
-    a.append([mx//count,my//count])
-    if(count>1):
-        i=step+1
-    else:
-        i=i+1
 
 
-########################## extracting character patch ###########################
+    ip=values[0]
+    ip=int(ip)
+    op_code = encode(ip)
+    print(op_code)
+    
+    if(values[1]=='yes'):
+        done=True
+    
+    l=int(crop_img.shape[0]/2)
+    w=int(crop_img.shape[1]/2)
+    
+    templates = splitting(crop_img)
+    mat = []
+    for template in templates:
+        t_w = int(template.shape[0]/2)
+        t_h = int(template.shape[1]/2)
+        im_pad = np.pad(img,((t_w,t_w),(t_h,t_h)),'constant')
+        ret=cv2.matchTemplate(im_pad,template,cv2.TM_CCORR_NORMED)
+        filtered = ndimage.maximum_filter(ret, size=4)
+        mat.append(filtered)
 
-templates = splitting(crop_img)
-mat = []
-for template in templates:
-    t_w = int(template.shape[0]/2)
-    t_h = int(template.shape[1]/2)
-    im_pad = np.pad(img,((t_w,t_w),(t_h,t_h)),'constant')
-    ret=cv2.matchTemplate(im_pad,template,cv2.TM_CCORR_NORMED)
-    filtered = ndimage.maximum_filter(ret, size=4)
-    mat.append(filtered)
+    s = np.array(mat[0])
+    for i in range(1,len(mat)):
+        s = s+np.array(mat[i])
 
-s = np.array(mat[0])
-for i in range(1,len(mat)):
-    s = s+np.array(mat[i])
+    op=np.zeros(ret.shape)
+    
+    ret=cv2.matchTemplate(img,crop_img,cv2.TM_CCORR_NORMED)
+    for i in range(ret.shape[0]):
+        for j in range(ret.shape[1]):
+            if(ret[i][j]>0.75):
+                op[i][j]=255
 
-op=np.zeros(ret.shape)
-for i in range(ret.shape[0]):
-    for j in range(ret.shape[1]):
-        if(ret[i][j]>0.75):
-            op[i][j]=255
-
-plt.imshow(op,'gray')
+    plt.imshow(op,'gray')
+    plt.show()
+    
+    index=np.where(op==255)
+    index=np.asarray(index)
+    index.sort()
 
 
-######################### HOG feature matching##################################
-
-fd_img, hog_image = hog(img, orientations=9, pixels_per_cell=(8, 8),
+    t=10
+    a=[]
+    i=0
+    
+    fd_img, hog_image = hog(img, orientations=9, pixels_per_cell=(8, 8), 
                     cells_per_block=(4, 4), visualize=True, multichannel=False)
-fd_temp, hog_temp = hog(crop_img, orientations=9, pixels_per_cell=(8, 8),
+    fd_crop, hog_temp = hog(crop_img, orientations=9, pixels_per_cell=(8, 8), 
                     cells_per_block=(4, 4), visualize=True, multichannel=False)
 
-plt.imshow(hog_image,'gray')
-plt.show()
-plt.figure(figsize=(2,2))
-plt.imshow(hog_temp,'gray')
-plt.show()
+    while(i<index.shape[1]):
+        print(i)
+        mx=index[0][i]
+        my=index[1][i]
+        count=1
+        for j in range(i+1,index.shape[1]):
+            marginx=np.abs(index[0][i]-index[0][j])
+            marginy=np.abs(index[1][i]-index[1][j])
+            if(marginx<t and marginy<t):
+                mx=mx+index[0][j]
+                my=my+index[1][j]
+                count=count+1
+                step=j
+        print(count,step)
+        a.append([mx//count,my//count])
+        if(count>1):
+            i=step+1
+        else:
+            i=i+1
+
+    a=np.asarray(a)
+    print(a.shape)
+    
+    match_scores=[]
+    patches=[]
+    match_coord = []
+    print(a.shape[0])
+    for i in range(a.shape[0]):
+        r,w = crop_img.shape
+        l1=int(r/2)
+        w1=int(w/2)
+        x=a[i][0]
+        y=a[i][1]
+        print(r,w)
+        print(l1,w1)
+
+        lr = x
+        hr = x+r
+        lw = y
+        hw = y+w
+
+        patch = img[lr:hr,lw:hw]
+        
+        fd_patch, hog_patch = hog(patch, orientations=9, pixels_per_cell=(8, 8), 
+                    cells_per_block=(4, 4), visualize=True, multichannel=False)
+        norm1=np.linalg.norm(fd_patch,2)
+        norm2=np.linalg.norm(fd_crop,2)
+        product = np.multiply(fd_patch,fd_crop).sum()/(norm1*norm2)
+    #     print(product) 
+        if product >= 0.5:
+            match_scores.append(product)
+            patches.append(patch)
+            match_coord.append([lr,hr,lw,hw])
+
+    print(match_scores)
+    match_scores = np.array(match_scores)
+    sort = match_scores.argsort()
+    print("sorted",sort)
+    for i in range(len(sort)):
+        print(match_scores[sort[i]])
+        plt.imshow(patches[sort[i]],'gray')
+        plt.show()
+        
+    #patches=np.asarray(patches)
+    for i in range(len(patches)):
+        img[match_coord[i][0]:match_coord[i][1], match_coord[i][2]:match_coord[i][3]] = 0
+        
+
 
 ############################## file generation #############################
 
@@ -305,7 +337,6 @@ for i in range(a.shape[0]):
 
 f.close()
 ############################### editable text generation ###################
-from __future__ import unicode_literals
 import numpy as np
 Info1 = open('dataa.txt','r')
 c = 0
